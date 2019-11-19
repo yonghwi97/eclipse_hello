2baa47fd3658a4c19798c0a9e94a99c2abe28fad
Yonghwi <hiw97@naver.com>
2019-11-19 21:28:57 +0900

    hotfix branch change a code

--- src/class2.java
+++ src/class2.java
@@ -3,6 +3,7 @@ public class class2 {
 	public static void main(String[] args) {
 		System.out.println("Wonkwang Univ. Computer SW department");
 		System.out.println("2'nd grade");
+		System.out.println("20162878");
 		}
 		}
 
