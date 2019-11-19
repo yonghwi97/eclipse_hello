e62e6342d7454fb423f23fa149fcab59cfdca489
yonghwi97 <55828458+yonghwi97@users.noreply.github.com>
2019-11-19 22:28:40 +0900

    Update PushPrint.java

--- src/PushPrint.java
+++ src/PushPrint.java
@@ -2,10 +2,7 @@
 public class PushPrint {
 	public static void main(String[] args) {
 		System.out.println("Add Java Class File Push");
-<<<<<<< HEAD
 		System.out.println("World with Miku");
-=======
 		System.out.println("World with 42");
->>>>>>> refs/remotes/origin/master
 	}
 }
